2815fd0d675a83749c7de4d527377081f3f81824
- <sandeepkumarhari@dxc.com>
2022-02-20 23:01:31 +0530

    HTTP is added

--- server.js
+++ server.js
@@ -1,31 +1,44 @@
-const express=require('express');
-const http = require('https');
-const app=express();
-const bodyParser=require('body-parser');
-app.set('view engine', 'ejs');
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended:true}));
+const express = require("express");
+const http = require("https");
+const app = express();
+const bodyParser = require("body-parser");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+let ipDetails;
+let Address,locate,zone,isp;
 
-app.get('/',(req,res)=>{
-res.render('map')
-})
+app.get("/", (req, res) => {
+  res.render("map");
+});
 
+app.post("/", (req, res) => {
+  const apiKey = "at_tYEdbDb3AQLXfkaFb3QekqClJSL84";
+  const ipAddress = req.body.ipaddress;
+ const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`;
+//   let Address = "";
+//   let locate = "";
+//   let zone = "";
+//   let isp = "";
 
-app.post("/",(req,res)=>{
-    console.log(req.body);
-    
+ http.get(url,(result)=>{
+    result.on('data',(data)=>{
+        ipDetails = JSON.parse(data);
+        Address = ipDetails.ip;
+        locate = ipDetails.location.city;
+        zone = ipDetails.location.timezone;
+        isp = ipDetails.isp;
+    })
+ }).on('error',(e)=>{
+     console.log(e);
+ })
 
-var ip = req.body.ipaddress;
-var api_key = 'at_tYEdbDb3AQLXfkaFb3QekqClJSL84';
-var api_url = 'https://geo.ipify.org/api/v1?';
+  res.render("mapResult", {
+    Address: Address,
+    locate: locate,
+    zone: zone,
+    isp: isp,
+  });
+});
 
-var url = api_url + 'apiKey=' + api_key + '&ipAddress=' + ip;
-
-http.get(url, function(response) {
-    var str = '';
-    response.on('data', function(chunk) { str += chunk; });
-    response.on('end', function() { console.log(str); });
-}).end();
-})
-
-app.listen(3000,console.log("Server listening"))
+app.listen(3000, console.log("Server listening"));
